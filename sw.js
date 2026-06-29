17fff6822b2afe876038bd48453f80acbc3292f6
Viktor-gym <for.ai.models.work@gmail.com>
2026-06-29 10:34:11 +0300

    Revert "Refine workout planner UX"
    This reverts commit 92638468ea2ef0aeead65a818f72f5926bad66dc.

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
 // - cache bust via CACHE version
 
 // bump version when you publish updates
-const CACHE = "gym-pwa-v29-planner-ux";
-const ASSETS = ["./index.html", "./app-v29.js", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE = "gym-pwa-v28-fix-exercises-records";
+const ASSETS = ["./index.html", "./app-v28.js", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
